bc6ad63e77975f23e3fdd49220a2c98aa0e85b8b
isabelledouin <isabelle.douin@orange.fr>
2022-01-23 17:15:39 +0100

    mise en place CRUD table Book

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,45 @@
+class BooksController < ApplicationController
+  before_action :set_book, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @books = Book.all
+  end
+
+ def new
+    @book = Book.new
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.save
+    redirect_to books_path
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to books_path
+  end
+
+  def destroy
+    @book.destroy
+    redirect_to books_path
+  end
+
+  private
+
+  def set_book
+    @book = Book.find(params[:id])
+  end
+
+  def book_params
+    params.require(:book).permit(:title, :author, :year, :resume, :price)
+  end
+
+end
